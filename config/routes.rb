063fcfbd48ec539f1ab87694b9d56c770ffcac0b
Shu52 <shusajid@gmail.com>
2022-02-14 16:13:30 -0600

    added flash, finished part 10

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
   # GET /about to:controller
-  get "about-us", to: "about#index", as: about
+  get "about-us", to: "about#index", as: :about
   # root is the same as get "/" or ""
   root to: "main#index"
 end
