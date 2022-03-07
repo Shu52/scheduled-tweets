496f0443b9188a8a388d3663e11f8624d0b80110
Shu52 <shusajid@gmail.com>
2022-03-07 13:30:26 -0600

    If you try to test validation in the middle of the video, it will error out. Working theory is that the latest version requires the format attribute is included  with validates email

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@
 class User < ApplicationRecord
     # has_secure_password is a method from rails https://api.rubyonrails.org/classes/ActiveModel/SecurePassword/ClassMethods.html
     has_secure_password
+    # 1 out 2 part to ensure that user is created with the email field filled. Other part in db migrate
+    validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "must be a valid email address" }
 end
--- db/migrate/20220303202919_create_users.rb
+++ db/migrate/20220303202919_create_users.rb
@@ -1,7 +1,8 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
-      t.string :email
+      # 2nd part of to ensure email field is completed. DB layer. Other part in user model
+      t.string :email, null: false
       t.string :password_digest
 
       t.timestamps
